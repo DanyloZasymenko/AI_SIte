de425c32204aef9e56ceb0d894e87803499c4a39
unknown <danik1492@gmail.com>
2018-11-22 23:58:01 +0200

    file preview

--- ai_site/static/script.js
+++ ai_site/static/script.js
@@ -8,3 +8,31 @@ function deleteWithChildrenModal(event) {
     $('#deleteOnlyModalLink').attr("href", $(event.target).data('link'));
     $('#deleteChildrenModalLink').attr("href", $(event.target).data('children-link'));
 }
+
+function filePreview(event) {
+    let input = event.target;
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            $('#preview').show();
+            $('#preview').attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function multipleFilePreview(event) {
+    $('#multiple-preview').text('');
+    let input = event.target;
+    if (input.files) {
+        let number = input.files.length;
+        for (i = 0; i < number; i++) {
+            let reader = new FileReader();
+            reader.onload = function (e) {
+                $($.parseHTML('<img class="preview-img">')).attr('src', e.target.result)
+                    .appendTo($('#multiple-preview')).show();
+            };
+            reader.readAsDataURL(input.files[i]);
+        }
+    }
+}
