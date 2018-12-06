3e193b40fb3463fc3f1bfa12b851fc6be8d9a06f
YKhemych <yosyp0108khemych@gmail.com>
2018-12-06 16:20:32 +0200

    Home page and News page

--- ai_site/routes/home_routes.py
+++ ai_site/routes/home_routes.py
@@ -1,13 +1,15 @@
-from flask import render_template
+from flask import render_template, request
 
 from ai_site import app
-from ai_site.models.news import News
+from ai_site.models.news import News, NewsCategory
+from ai_site.models.page import Page, PageText
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", title='Home')
+    print(Page.query.filter_by(name = "home").first())
+    return render_template("home.html", title='Home', page = Page.query.filter_by(name = "home").first())
 
 
 @app.route("/admin")
@@ -15,6 +17,19 @@ def admin():
     return render_template("admin.html")
 
 
-@app.route("/news")
-def news():
-    return render_template('news.html', title='News', news_list=News.query.all())
+@app.route("/news/<int:category_id>/page/<int:page_number>")
+def news(category_id, page_number):
+    if category_id == 0:
+        page = request.args.get('page', page_number, type=int)
+        news = News.query.order_by(News.date_posted.desc()).paginate(page=page, per_page=6)
+        return render_template('news.html', title='News',category_id = category_id,  news_list=news, categories=NewsCategory.query.all())
+    else:
+        page = request.args.get('page', page_number, type=int)
+        news = News.query.filter_by(category_id = category_id).order_by(News.date_posted.desc()).paginate(page = page, per_page= 6)
+        return render_template("news.html", title='News', category_id=category_id, news_list=news, categories=NewsCategory.query.all())
+
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html", title='Contacts')
+
